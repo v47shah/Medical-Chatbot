6164a3e82f20073cd2fdd426aa7a44e6fd8e5078
Vidit Shah <84160951+v47shah@users.noreply.github.com>
2025-12-21 22:23:32 -0500

    fixed port

--- app.py
+++ app.py
@@ -264,4 +264,5 @@ def chat():
 # Run
 # =========================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
+
